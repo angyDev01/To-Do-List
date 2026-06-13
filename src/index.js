305409f1a9e44feb0fdc5ca04fc4a3d031d88a7f
Angy <yaojeanangoa@gmail.com>
2026-06-13 02:20:57 +0000

    Mise à jour du js au niveau des taches à supprimer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const TextInput = document.getElementById("TextInput1");
 const taskContainer = document.getElementById("contentContainer")
 
 var WorkList = JSON.parse(localStorage.getItem('tasks')) || [];
+var Taskfinished = JSON.parse(localStorage.getItem('tasks'));
+
 renderTasks();
 
 function AddTask(){
@@ -31,6 +33,12 @@ function renderTasks(){
     if (e.target.tagName === 'LI'){
         var lI = e.target.classList;
         lI.toggle('checked');
+
+        if (lI.value === "checked" && e.target.tagName === 'LI')
+            alert(1234)
+            let value = e.target.firstChild.textContent
+            Taskfinished.push(value)
+
     }else if(e.target.tagName === 'SPAN'){
         // Récuperation de la tache
         const li = e.target.parentElement;
@@ -38,17 +46,18 @@ function renderTasks(){
 
         //  Rétrouver le même texte/tache pour le supprimer de la liste
         WorkList = WorkList.filter(task => task !== taskText);
+        Taskfinished = Taskfinished.filter(task => task !== taskText)
 
         // Mise à jour du localStorage
         localStorage.setItem("tasks", JSON.stringify(WorkList));
 
+
         // Supprimer la tache du DOM
         li.remove()
+
+        //ajouter le nombre de taches terminées et celles en cours
+
+        
     }
 })
 }
-
-
-
-
-
